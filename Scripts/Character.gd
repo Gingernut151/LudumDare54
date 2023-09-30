01ffe4cc922e317c583360b179e16c991a933cd4
andrew <androx360@Gmail.com>
2023-09-30 18:19:23 +0100

    Animations Applied

--- Scripts/Character.gd
+++ Scripts/Character.gd
@@ -7,6 +7,8 @@ var CharacterHeight: float = 100.0
 var CurrentAmountToMove: float = -1.0
 var CurrentMoveVector: Vector2
 
+@onready var _animated_sprite = $AnimatedSprite2D
+
 func _physics_process(delta):
 	handleInput(delta)
 	move_and_slide()
@@ -29,6 +31,15 @@ func handleInput(delta):
 
 func GetInputDirectionPressed():
 	var moveDirection = Input.get_vector("ui_left", "ui_right", "ui_up", "ui_down")
+	
+	if Input.is_action_pressed("ui_right"):
+		_animated_sprite.play("walk")
+		_animated_sprite.flip_h = false
+	elif Input.is_action_pressed("ui_left"):
+		_animated_sprite.play("walk")
+		_animated_sprite.flip_h = true
+	else:
+		_animated_sprite.play("idle")
 	
 	if (abs(moveDirection.length()) != 1.0):
 		moveDirection = Vector2.ZERO
