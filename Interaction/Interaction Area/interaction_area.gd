6a0cb6a2f4d6aaebb2b00db3a6b19e357bb407a6
andrew <androx360@Gmail.com>
2023-10-02 18:37:20 +0100

    Interaction System

---
+++ Interaction/Interaction Area/interaction_area.gd
@@ -0,0 +1,18 @@
+extends Area2D
+class_name InteractionArea
+
+@export var action_name: String = "Interact"
+
+
+var interact: Callable = func():
+	pass
+	
+
+func _on_area_entered(area: Area2D) -> void:
+	InteractionManager.register_area(self)
+
+
+
+
+func _on_area_exited(area: Area2D) -> void:
+	InteractionManager.unregister_area(self)
--- Scripts/Environment/Grave.gd
+++ Scripts/Environment/Grave.gd
@@ -4,31 +4,42 @@ var GraveWriting : String = ""
 
 var inArea : bool = false
 
+@onready var interaction_area: InteractionArea = $interaction_area
+
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
+	interaction_area.interact = Callable(self, "_on_interact")
 
 
  #Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	if inArea && Input.is_action_pressed("Interact"):
-		var OwnerNode = get_node("/root/World")
-		OwnerNode.HandleGameOverState(true)	
+	pass
+#	if inArea && Input.is_action_pressed("Interact"):
+#		var OwnerNode = get_node("/root/World")
+#		OwnerNode.HandleGameOverState(true)	
 
 func SetGraveWritting(InWritting):
 	GraveWriting = InWritting
 
-func _on_area_entered(area):
+func _on_interact():
 	var OwnerNode = get_node("/root/World")
 	OwnerNode.OnGraveHit(true, GraveWriting)
-	inArea = true
-	print(inArea)
-	
-func _on_area_exited(area):
-	var OwnerNode = get_node("/root/World")
-	OwnerNode.OnGraveHit(false, "")
-	inArea = false
-	print(inArea)
+	print("hit")
+
+
+
+
+#func _on_area_entered(area):
+#	var OwnerNode = get_node("/root/World")
+#	OwnerNode.OnGraveHit(true, GraveWriting)
+#	inArea = true
+#	print(inArea)
+#
+#func _on_area_exited(area):
+#	var OwnerNode = get_node("/root/World")
+#	OwnerNode.OnGraveHit(false, "")
+#	inArea = false
+#	print(inArea)
 
 #func _on_input_event(viewport, event, shape_idx):
 #	var OwnerNode = get_node("/root/World")
